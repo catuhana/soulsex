27ff142ef51db4116573454573ec8f48ff2c96f8
tuhana <tuhana.cat+git@gmail.com>
2026-05-30 14:07:04 +0300

    Add dialyxir for Dialyzer task

--- mix.exs
+++ mix.exs
@@ -13,6 +13,7 @@ defmodule SoulsexUmbrella.MixProject do
   defp deps do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
     ]
   end
 end
