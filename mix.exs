1cecee2cc0ff3f42149c67dc9c3c382ab6ccdb5d
tuhana <tuhana.cat+git@gmail.com>
2026-05-30 14:23:36 +0300

    Run `nix fmt`

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule SoulsexUmbrella.MixProject do
   defp deps do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 end
