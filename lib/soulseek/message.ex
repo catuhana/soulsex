5caa8b172b63b0904faf59144297f0c96dedf0ec
tuhana <tuhana.cat+git@gmail.com>
2026-07-01 23:51:52 +0000

    Alias `struct()` to `t()` on `Soulseek.Message`

--- lib/soulseek/message.ex
+++ lib/soulseek/message.ex
@@ -1,6 +1,8 @@
 defmodule Soulseek.Message do
   @moduledoc "Behaviour for protocol messages that encode to and decode from the wire."
 
-  @callback encode(struct()) :: iodata()
-  @callback decode(binary()) :: struct()
+  @type t :: struct()
+
+  @callback encode(t()) :: iodata()
+  @callback decode(binary()) :: t()
 end
